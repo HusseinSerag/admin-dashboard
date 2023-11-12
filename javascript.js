46f63f68c8d75d76706920090301c3d382fc1e64
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2023-11-12 17:47:19 +0200

    Finish sidebar on desktop screens
    sidebar is totally finished and implemented as grid

--- javascript.js
+++
@@ -1,7 +0,0 @@
-
-
-const dashboard = document.querySelector('nav h1');
-const navlinks = document.querySelector('nav .nav')
-dashboard.addEventListener('click', ()=>{
-    navlinks.classList.toggle('active')
-})
